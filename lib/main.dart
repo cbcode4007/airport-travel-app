c96e842cd57057e1fb7b257c66c2c0972d2cc6f6
Colin Bond <colin.bond@dcmail.ca>
2025-05-01 15:12:03 -0400

    Test Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,42 @@
   File:        main.dart
   
   Description: This file is at the center of the Airport Travel Application, being responsible for
-               running it and navigating to its home page.
+               running it and initially navigating to a specific page after checking a session exists.
 */
 
 // Imported dependency packages.
-// Material app design, or in other words Google standards for UI.
-import 'package:airport_travel_app/firebase_options.dart';
-import 'package:airport_travel_app/screen/passport.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-// Timezone logic for correct displays.
-import 'package:timezone/data/latest.dart' as tz;
-// Route to the next screen.
-import 'package:airport_travel_app/screen/auth_gate.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+// Material app design, or in other words Google standards for UI.
+import 'package:flutter/material.dart';
+
+// Firebase suite for seamless authentication.
+// Configuration.
+import 'package:airport_travel_app/firebase_options.dart';
+// Account functionality.
+import 'package:firebase_auth/firebase_auth.dart';
+// Database storage functionality.
+import 'package:firebase_core/firebase_core.dart';
+
+// Timezone logic for correct displays initialization.
+import 'package:timezone/data/latest.dart' as tz;
+
+// Routes to the possible initial screens.
+// Authentication gate.
+import 'package:airport_travel_app/screen/auth_gate.dart';
+// Passport setup.
+import 'package:airport_travel_app/screen/passport.dart';
+
+
+// Central Firebase client ID to pass into the authentication gate for verification on the app side.
 const clientId = '299611344278-kq29k1540bnkr9v5vchnml3ub35d7bcs.apps.googleusercontent.com';
 
+// Initialize authentication, timezone and advertisement databases before running the app so they can load.
 void main () async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   tz.initializeTimeZones();
-  MobileAds.instance.initialize();
-
   runApp(const AirportTravelApp());
 }
 
@@ -38,28 +48,25 @@ class AirportTravelApp extends StatelessWidget {
   // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
-    // return MaterialApp(
-    //   // Navigate to the first or Welcome page.
-    //   // home: const LoginPage(),
-    //   home: const AuthGate(),
-    //   theme: ThemeData(
-    //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue)
-    //   ),
-    // );
     return MaterialApp(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue)
       ),
+      // Dynamically loaded page that listens to state changes of Firebase authentication.
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
+            // Loading screen.
             return const Center(child: CircularProgressIndicator());
-          }
+          } 
+          // When the user restarts the app, open a different page depending on their last login status.         
           if (snapshot.hasData) {
-            return const PassportPage(); // or your home screen
+            // Redirect to the first page in the information setup process for a logged in user.
+            return const PassportPage();
           } else {
-            return const AuthGate(); // or your sign-in screen
+            // Go to the real first page requiring account credentials for a signed off user.
+            return const AuthGate();
           }
         },
       ),
--- lib/screen/auth_gate.dart
+++ lib/screen/auth_gate.dart
@@ -1,34 +1,84 @@
-import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
-import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+/*
+  Author:      Colin Bond
+  File:        auth_gate.dart
+  
+  Description: This file handles both logging in and signing up to the application, which is crucial to protect
+               sensitive information such as users' entered passports. It makes use of Google's Firebase to
+               provide a material interface for the page, a robust and free credential storage system, and Google
+               account support.
+*/
+
+// Imported dependency packages.
+
+// Material app design, or in other words Google standards for UI.
 import 'package:flutter/material.dart';
 
+// Firebase suite for seamless authentication.
+// Account functionality with email sign in provision.
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+// Adapted user interface login/signup screen which presents many of the possibilities of Firebase.
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+// Google account sign in option.
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+
+// Provide the app client ID.
+import 'package:airport_travel_app/main.dart';
+
+// Routes to the other screens.
+// Passport setup.
 import 'passport.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
+  // Only go through with loading the authentication form if not already logged in, and redirect after it all.
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
-          
+
+          // The provided page with standard material design and high functionality.
           return SignInScreen(
+            // Authentication methods.
             providers: [
               EmailAuthProvider(),
-              GoogleProvider(clientId: '299611344278-kq29k1540bnkr9v5vchnml3ub35d7bcs.apps.googleusercontent.com'),
+              GoogleProvider(clientId: clientId),
             ],
-            actions: [
+            // Upon failure to authenticate, capture the error code to inform both developer and user.
+            actions: [              
               AuthStateChangeAction<AuthFailed>((context, state) {
                 final error = state.exception;
-                print("Authentication failed: $error");
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Authentication failed: $error')),
+                // Creates a visually striking error message tailored to the situation front and center.
+                showDialog<void>(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Error'),
+                      content: SingleChildScrollView(
+                        child: ListBody(
+                          children: <Widget>[
+                            Text(error.toString()),
+                          ],
+                        ),
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                          child: const Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
                 );
               }),
             ],
+            // The rest of the visual appearance of the app.
+            // Company logo.
             headerBuilder: (context, constraints, shrinkOffset) {
               return Padding(
                 padding: const EdgeInsets.all(20),
@@ -38,6 +88,7 @@ class AuthGate extends StatelessWidget {
                 ),                
               );
             },
+            // Introduction label.
             subtitleBuilder: (context, action) {
               return Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8.0),
@@ -46,6 +97,7 @@ class AuthGate extends StatelessWidget {
                     : const Text('Welcome to IxIxI Creator Labs Airport Travel App! Please sign up.'),
               );
             },
+            // Terms label.
             footerBuilder: (context, action) {
               return const Padding(
                 padding: EdgeInsets.only(top: 16),
@@ -55,6 +107,7 @@ class AuthGate extends StatelessWidget {
                 ),
               );
             },
+            // Naturally shrink the logo and keep it visible when scrolling down the page.
             sideBuilder: (context, shrinkOffset) {
               return Padding(
                 padding: const EdgeInsets.all(20),
@@ -66,6 +119,7 @@ class AuthGate extends StatelessWidget {
             },
           );
         }
+        // Once the user passes authentication, navigate to passport setup.
         return const PassportPage();
       },
     );
